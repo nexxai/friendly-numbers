de450c10ef31f026613f822b89111a720b612b52
JT Smith <nexxai@gmail.com>
2021-11-11 11:35:12 -0700

    Initial commit

---
+++ friendly.py
@@ -0,0 +1,73 @@
+# friendly.py - v1.0
+#
+# Check for friendly numbers as described here: https://www.youtube.com/watch?v=KZ1BVlURwfI
+# 
+# Features:
+# - Checkpoint saving so the script can be exited and resumed with little loss of work
+# 
+# To-Do:
+# - Multithreading?
+# - Use shaders for calculations?
+
+from datetime import datetime
+from sympy import divisors
+import sys, os
+
+# See if there's a progress file...
+progress_file_name = "progress.txt"
+if os.path.isfile(progress_file_name):
+    # If so, open it...
+    progress_file = open(progress_file_name, "r")
+    if progress_file.mode == 'r':
+        # and get the current checkpoint
+        start_num = progress_file.read()
+        
+        # If it contains the text 'FOUND' just print it out and exit
+        if start_num.startswith('FOUND'):
+            print(start_num)
+            print(divisors(start_num))
+            sys.exit()
+        # Otherwise we're loading in that checkpoint
+        start_num = int(start_num)
+else:
+    # No progress file found, so just start at 1 trillion
+    start_num = 10**12
+
+# Just start looping through each number, one by one
+for num in range(start_num, 10**13):
+    # Get each of the factors from the current number
+    factors = divisors(num)
+
+    # Initialize the counter
+    sum_of_factors = 0
+    # and then add the factors together
+    for factor in factors:
+        sum_of_factors += factor
+
+    # Calculate the index by dividing the sum of the factors by the number we're working on
+    index = sum_of_factors / num
+
+    # If it matches 1.8 (the same as '10'), we found one so break out of the loop
+    if index == 1.8:
+        print(f'{sum_of_factors} / {num} = {index}')
+        print(factors)
+        break
+
+    # Just give the user some visual feedback that we haven't stalled
+    # so every 10000 numbers we check, show the status on the screen
+    if num % 10**4 == 0:
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+        print(f'[{current_time}] Working on: {num}')
+        print(f'{sum_of_factors} / {num} = {index}')
+        print(factors)
+
+        # And update the checkpoint file
+        progress = open(progress_file_name, "w")
+        progress.write(str(num))
+        
+# We found one so update the checkpoint file with the details
+print('FOUND ONE!')
+progress = open(progress_file_name, "w")
+progress.write(f'FOUND: {str(num)}')
